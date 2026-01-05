d28f422336b9dfc1786cab8fb30e390a9ace27f7
raymondmg <wang.xu@ist.hokudai.ac.jp>
2026-01-05 12:31:03 +0900

    update

--- scripts/run-demo-aligning-skel2smpl.bat
+++ scripts/run-demo-aligning-skel2smpl.bat
@@ -1,5 +1,5 @@
 @echo off
 cd ..
 
-uv run python examples/align_to_SMPL_frame.py --smpl_data 'examples/samples/img_fit/emily-sea-coiWR0gT8Cw-unsplash_0.npz' 
+uv run python examples/align_to_SMPL_frame.py --smpl_data "./examples/samples/img_fit/emily-sea-coiWR0gT8Cw-unsplash_0.npz"
 pause
